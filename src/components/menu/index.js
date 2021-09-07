5abebebd141d409989344136df6b48c2ee36c3d8
LaryssaGomes <laryssag533@gmail.com>
2021-09-07 10:22:55 -0300

    Melhoramento na responsividade

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,29 +1,41 @@
 import "./styles.css";
+import { useState } from "react";
 const Menu = () => {
+  const [itemsChange, setItemsChange] = useState(false);
+  function menuOnClick() {
+    const status = itemsChange ? false : true;
+    setItemsChange(status);
+  }
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" id="nav-link-main" href="/">
-              Padawans
-            </a>
-          </li>
-          <li className="nav-item active">
-            <a className="nav-link" id="nav-link" href="/todos">
-              Todos
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" id="nav-link" href="/albums">
-              Albums
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" id="nav-link" href="/posts">
-              Postagens
-            </a>
-          </li>
+    <nav id="navbar" className={itemsChange ? "change" : "no-change"}>
+      <div id="menu-bar" onClick={menuOnClick}>
+        <div id="bar1" className="bar"></div>
+        <div id="bar2" className="bar"></div>
+        <div id="bar3" className="bar"></div>
+      </div>
+      <div>
+        <ul className="navbar-links">
+          <div>
+            <li className="nav-item active">
+              <a id="link-main" href="/">
+                Padawans
+              </a>
+            </li>
+          </div>
+          <div>
+            <li className="nav-item active">
+              <a href="/">Home</a>
+            </li>
+            <li className="nav-item active">
+              <a href="/todos">Todos</a>
+            </li>
+            <li className="nav-item">
+              <a href="/albums">Albums</a>
+            </li>
+            <li className="nav-item">
+              <a href="/posts">Postagens</a>
+            </li>
+          </div>
         </ul>
       </div>
     </nav>
